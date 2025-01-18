16822919084f2979b5c7ade20f09f3638253da0a
hbybyyang <2451759073@qq.com>
2025-01-19 05:04:52 +0800

    允许使用更多模型

--- run.cmd
+++ run.cmd
@@ -18,7 +18,6 @@ if "%STAGE%"=="安装环境" goto :INSTALL_ENV
 if "%STAGE%"=="应用补丁" goto :PATCH
 if "%STAGE%"=="进入环境" goto :ACTIVATE_ENV
 if "%STAGE%"=="安装依赖" goto :INSTALL_DEPENDENCIES
-if "%STAGE%"=="下载模型" goto :DOWNLOAD_MODELS
 if "%STAGE%"=="运行" goto :RUN
 
 :SUBMODULE
@@ -37,6 +36,7 @@ if %ERRORLEVEL% neq 0 echo 致命错误 && pause && exit
 echo 应用补丁
 echo 应用补丁>%STAGE_FILE%
 copy .\patch\requirements.txt .\CosyVoice\
+copy .\patch\webui.py .\CosyVoice\
 if %ERRORLEVEL% neq 0 echo 致命错误 && pause && exit
 
 :ACTIVATE_ENV
@@ -53,25 +53,46 @@ if %ERRORLEVEL% neq 0 echo 致命错误 && pause && exit
 cd CosyVoice && pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple/ --trusted-host=mirrors.aliyun.com && cd ..
 if %ERRORLEVEL% neq 0 echo 致命错误 && pause && exit
 
-:DOWNLOAD_MODELS
-echo 下载模型
-echo 下载模型>%STAGE_FILE%
-if not exist pretrained_models mkdir pretrained_models
-if not exist pretrained_models\CosyVoice-300M-SFT git clone https://www.modelscope.cn/iic/CosyVoice-300M-SFT.git pretrained_models/CosyVoice-300M-SFT
-if %ERRORLEVEL% neq 0 echo 致命错误 && pause && exit
-if not exist pretrained_models\CosyVoice2-0.5B git clone https://www.modelscope.cn/iic/CosyVoice2-0.5B.git pretrained_models/CosyVoice2-0.5B
-if %ERRORLEVEL% neq 0 echo 致命错误 && pause && exit
-
 :RUN
 echo 请选择模型:
-echo "1: 使用 SFT 模型（具有预训练音色）"
-echo "2: 用 V2 模型（只能进行语音复刻）"
-set /p modelChoice="请输入选择的数字 (1 或 2): "
+echo "1: CosyVoice2-0.5B"
+echo "2: CosyVoice-300M"
+echo "3: CosyVoice-300M-25Hz"
+echo "4: CosyVoice-300M-SFT"
+echo "5: CosyVoice-300M-Instruct"
+set /p modelChoice="请输入选择的数字: "
 if exist "%USERPROFILE%\.cache\modelscope" rmdir /s /q "%USERPROFILE%\.cache\modelscope"
 call .\OCR-portable-conda\Scripts\activate.bat
-if "%modelChoice%"=="1" python .\CosyVoice\webui.py --model_dir pretrained_models\CosyVoice-300M-SFT
-if "%modelChoice%"=="2" python .\CosyVoice\webui.py --model_dir pretrained_models\CosyVoice2-0.5B
-if not "%modelChoice%"=="1" if not "%modelChoice%"=="2" echo 无效的选择，请重新运行程序并选择正确的模型 && pause && exit
-if %ERRORLEVEL% neq 0 echo 致命错误 && pause && exit
+if not exist pretrained_models mkdir pretrained_models
+if "%modelChoice%"=="1" (
+    if not exist pretrained_models\CosyVoice2-0.5B git clone https://www.modelscope.cn/iic/CosyVoice2-0.5B.git pretrained_models/CosyVoice2-0.5B
+    if %ERRORLEVEL% neq 0 echo 致命错误 && pause && exit
+    python .\CosyVoice\webui.py --model_dir pretrained_models\CosyVoice2-0.5B
+)
+if "%modelChoice%"=="2" (
+    if not exist pretrained_models\osyVoice-300M git clone https://www.modelscope.cn/iic/CosyVoice-300M.git pretrained_models/CosyVoice-300M
+    if %ERRORLEVEL% neq 0 echo 致命错误 && pause && exit
+    python .\CosyVoice\webui.py --model_dir pretrained_models\CosyVoice-300M
+)
+if "%modelChoice%"=="3" (
+    if not exist pretrained_models\CosyVoice-300M-25Hz git clone https://www.modelscope.cn/iic/CosyVoice-300M-25Hz.git pretrained_models/CosyVoice-300M-25Hz
+    if %ERRORLEVEL% neq 0 echo 致命错误 && pause && exit
+    python .\CosyVoice\webui.py --model_dir pretrained_models\CosyVoice-300M-25Hz
+)
+if "%modelChoice%"=="4" (
+    if not exist pretrained_models\CosyVoice-300M-SFT git clone https://www.modelscope.cn/iic/CosyVoice-300M-SFT.git pretrained_models/CosyVoice-300M-SFT
+    if %ERRORLEVEL% neq 0 echo 致命错误 && pause && exit
+    python .\CosyVoice\webui.py --model_dir pretrained_models\CosyVoice-300M-SFT
+)
+if "%modelChoice%"=="5" (
+    if not exist pretrained_models\CosyVoice-300M-Instruct git clone https://www.modelscope.cn/iic/CosyVoice-300M-Instruct.git pretrained_models/CosyVoice-300M-Instruct
+    if %ERRORLEVEL% neq 0 echo 致命错误 && pause && exit
+    python .\CosyVoice\webui.py --model_dir pretrained_models\CosyVoice-300M-Instruct
+)
+if not "%modelChoice%"=="1" if not "%modelChoice%"=="2" if not "%modelChoice%"=="3" if not "%modelChoice%"=="4" if not "%modelChoice%"=="5" (
+    echo 无效的选择，请重新运行程序并选择正确的模型
+    pause
+    exit
+)
 
 pause
